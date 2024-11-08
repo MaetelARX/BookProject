d4db976c5804a6acfecf0e350f9e3d4fd9ceb27b
Plamen Jelev <pjelev8@gmail.com>
2024-11-08 23:59:43 +0200

    Creating the models
    In this repository I've created the models for my database as following:
    
    Book: The Book model contains a primary key Id, a name of the book which is required and on top of that is with Maximum length of 50 characters, a price of the book, an image we can add of the book, a GenreId which will be a Foreign key to the Genre Table, a collection of Genres, collection of OrderDetail, and collection of CartDetail.
    
    CartDetail: the Cart Detail contains of Id, ShoppingCartId which is primary key and required as well as BookId, Quantity, collection of Book and Collection of ShoppingCart
    
    Genre: Genre contains Id, name of the genre and collection of the Books
    
    Order: Order contains Id Primary Key, UserId which is Foreign Key, Date of the creation of the order, OrderStatus Id (Foreign Key), I use soft delete variable here named IsDeleted, collection of OrderStatus and collection of OrderDetail
    
    OrderDetail: OrderDetail contains a primary Key Id, 2 Foreign Keys OrderId and BookId, Quantity, Price of the order, collections of Order and Book
    
    OrderStatus: Contains Id, StatusId, and name of the Status
    
    ShoppingCart: Lastly ShoppingCart Contains Id, UserId ForeignKey and here I again apply the soft Delete method

---
+++ BookProject/BookProject/Models/Book.cs
@@ -0,0 +1,20 @@
+﻿using NuGet.Protocol.Core.Types;
+using System.ComponentModel.DataAnnotations;
+
+namespace BookProject.Models
+{
+    public class Book
+    {
+        public int Id { get; set; }
+        [Required]
+        [MaxLength(50)]
+        public string? BookName { get; set; }
+        public double Price { get; set; }
+        public string? Image {  get; set; }
+        [Required]
+        public int GenreId { get; set; }
+        public Genre Genre { get; set; }
+        public List<OrderDetail> OrderDetail { get; set; }
+        public List<CartDetail> CartDetail { get; set; }
+    }
+}
